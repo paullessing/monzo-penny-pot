21b2ae6b2a668eee86c36c19fd8bd46236096c86
paullessing <paul@paullessing.com>
2018-03-11 10:58:51 +0000

    Update webhook settings

--- handler.ts
+++ handler.ts
@@ -2,4 +2,12 @@ import { aws, HandlerRequest, HandlerResponse } from 'serverless-api-handlers';
 
 async function onWebhook(request: HandlerRequest): Promise<HandlerResponse> {
   // TODO: Get Monzo Types and respond to webhook
+  console.log(request);
+  return {
+    statusCode: 200
+  };
 }
+
+export = {
+  webhook: aws.wrap(onWebhook)
+};
